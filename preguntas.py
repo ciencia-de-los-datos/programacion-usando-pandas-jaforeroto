77077a8e10508dda34ab8a4ed32c499d68e644c8
Javier_Forero <112499361+jaforeroto@users.noreply.github.com>
2022-10-05 10:26:35 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -184,13 +184,13 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
-    x = tbl0.filter(items=('_c1','_c3'))
-    x = tbl0.sort_values('_c2')
-    x['_c2'] = x['_c2'].astype(str)
-    x = x.groupby('_c1').agg({'_c2':':'.join})
 
-    return x
+    k = tbl0.filter(items=('_c1','_c3'))
+    k = tbl0.sort_values('_c2')
+    k['_c2'] = k['_c2'].astype(str)
+    k = k.groupby('_c1').agg({'_c2':':'.join})
+
+    return k
 
 
 def pregunta_11():
